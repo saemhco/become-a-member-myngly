6301c5a14d07de5b97c6b3659db02a9cf8b5e436
Saúl Escandón <saulem1994@gmail.com>
2024-10-23 11:01:27 -0500

    add subscribe list

--- public/js/bam-myngly-public.js
+++ public/js/bam-myngly-public.js
@@ -20,8 +20,12 @@
                 processResults: function(data) {
                     return {
                         results: data.data.map(function(item) {
+							let itemKey = item[idKey];
+                            if (selector === '#from' || selector === '#lives_in') {
+								itemKey = item.name + ', ' + item.state_iso_code;
+							}
                             return {
-                                id: item[idKey],
+                                id: itemKey,
                                 text: item[textKey]
                             };
                         })
@@ -93,10 +97,12 @@
         initSelect2WithBackendSearch('#industry', '/api/typ/list-industry', 'search', 'id', 'name', 'Search and select industries...', true);
         initSelect2WithBackendSearch('#years_of_experience', '/api/typ/list-years-of-experience', 'search', 'id', 'name', 'Search and select your experience level...', false);
         initSelect2WithBackendSearch('#job_title', '/api/typ/list-jobs', 'search', 'name', 'name', 'Search and select your job title...', false);
-        initSelect2WithBackendSearch('#company', '/api/companies/search', 'query', 'id', 'name', 'Search and select your company...', false);
+        //initSelect2WithBackendSearch('#company', '/api/companies/search', 'query', 'name', 'name', 'Search and select your company...', false);
         initSelect2WithBackendSearch('#lives_in', '/api/typ/list-cities', 'search', 'name', 'name', 'Select your city...', false);
         initSelect2WithBackendSearch('#from', '/api/typ/list-cities', 'search', 'name', 'name', 'Select your city of origin...', false);
         initSelect2WithBackendSearch('#education', '/api/typ/list-universities', 'search', 'name', 'name', 'Select your university...', false);
+        initSelect2WithBackendSearch('#list_ids', '/api/typ/constant-contact/list', 'search', 'id', 'name', 'Click here...', true);
+
 
     }
 
@@ -118,95 +124,134 @@
 
     // Handle form submission
     function handleFormSubmission() {
-        $('#myngly-form').on('submit', function(e) {
+        $('#myngly-form').on('submit', function (e) {
             e.preventDefault();
             const phoneCode = $('#phone_code').val();
             const phoneNumber = $('#phone_number').val();
             const fullPhoneNumber = phoneCode + phoneNumber;
             const passions = $('#passions').val();
-			const currentGoals = $('#current_goals').val();
-			const educationLevel = $('#education_level').val();
-			const industries = $('#industry').val();
-			const yearsOfExperience = $('#years_of_experience').val();
-			const combinedFilterIds = currentGoals.concat(educationLevel, industries, yearsOfExperience);
-            
+            const currentGoals = $('#current_goals').val();
+            const educationLevel = $('#education_level').val();
+            const industries = $('#industry').val();
+            const yearsOfExperience = $('#years_of_experience').val();
+            const combinedFilterIds = currentGoals.concat(educationLevel, industries, yearsOfExperience);
+    
             if (passions.length !== 5) {
                 alert("Please select exactly 5 passions.");
                 return;
             }
-
-            Swal.fire({ title: 'Sending code...', text: 'Please wait while we send the code to your phone.', allowOutsideClick: false, didOpen: () => Swal.showLoading() });
-
+    
+            // Show loading message while verifying the data
+            Swal.fire(
+                { 
+                    title: 'Sending code...', 
+                    text: 'Please wait while we send the code to your phone: '+fullPhoneNumber, 
+                    allowOutsideClick: false, 
+                    didOpen: () => Swal.showLoading()
+                });
+    
             fetch(`${apiConfig.apiBaseUrl}/api/typ/sendcode`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ phone_number: fullPhoneNumber })
             })
-            .then(response => response.json())
-            .then(() => {
-                Swal.close();
-                Swal.fire({
-                    title: 'Enter the verification code',
-                    input: 'tel',
-                    inputAttributes: { maxlength: 6, inputmode: 'numeric' },
-                    showCancelButton: true,
-					didOpen: () => {
-						const input = Swal.getInput();
-						input.addEventListener('input', () => {
-							if (input.value.length === 6) {
-								Swal.clickConfirm();
-							}
-						});
-					},
-                    preConfirm: (code) => submitFormData(code, fullPhoneNumber, combinedFilterIds)
-                });
-            })
-            .catch(error => Swal.fire({ title: 'Error!', text: 'Failed to send verification code!', icon: 'error' }));
+                .then(response => response.json())
+                .then(() => {
+                    Swal.close();
+                    Swal.fire({
+                        title: 'Enter the verification code sent to ' + fullPhoneNumber,
+                        input: 'tel',
+                        inputAttributes: { maxlength: 6, inputmode: 'numeric' },
+                        showCancelButton: true,
+                        didOpen: () => {
+                            const input = Swal.getInput();
+                            input.addEventListener('input', () => {
+                                if (input.value.length === 6) {
+                                    Swal.clickConfirm();
+                                }
+                            });
+                        },
+                        preConfirm: (code) => submitFormData(code, fullPhoneNumber, combinedFilterIds)
+                    });
+                })
+                .catch(error => Swal.fire({ title: 'Error!', text: 'Failed to send verification code!', icon: 'error' }));
         });
     }
     // Function to submit form data
     function submitFormData(code, phoneNumber, combinedFilterIds) {
-		linkedinProfile = JSON.parse(localStorage.getItem('linkedin-profile'));
+        const linkedinProfileValue = $('#linkedin-profile').val();
+
+        let linkedinProfile = {};
+
+        // Attempt to parse LinkedIn profile if it's a string
+        if (typeof linkedinProfileValue === 'string') {
+            try {
+                linkedinProfile = JSON.parse(linkedinProfileValue);
+            } catch (error) {
+                console.error('Error parsing LinkedIn profile JSON', error);
+            }
+        } else if (typeof linkedinProfileValue === 'object') {
+            linkedinProfile = linkedinProfileValue;
+        }
+
         const formData = {
-			code: code,
-			name: $('#name').val(),
-			email: $('#email').val(),
-			phone_number: phoneNumber,
-			birthday_at: $('#birthday_at').val(),
-			gender: $('#gender').val(),
-			passions: Array.from(passions.selectedOptions).map(option => option.value),
-			linkedin_url: $('#linkedin_url').val(),
-			image_url: $('#image_url').val(),
-			filter_id: combinedFilterIds,
-			about_me: $('#about_me').val(),
-			lives_in: $('#lives_in').val(),
-			from: $('#from').val(),
-			job_titles: $('#job_title').val(),
-			company: $('#company').val(),
-			education: $('#education').val(),
-			military_veteran: $('#military_veteran').val(),
-			receive_news: $('#receive_news').val(),
-			linkedin: linkedinProfile,
+            code: code,
+            name: $('#name').val(),
+            email: $('#email').val(),
+            phone_number: phoneNumber,
+            birthday_at: $('#birthday_at').val(),
+            gender: $('#gender').val(),
+            passions: Array.from(passions.selectedOptions).map(option => option.value),
+            linkedin_url: $('#linkedin_url').val(),
+            image_url: $('#image_url').val(),
+            filter_id: combinedFilterIds,
+            about_me: $('#about_me').val(),
+            lives_in: $('#lives_in').val(),
+            from: $('#from').val(),
+            job_titles: $('#job_title').val(),
+            company: $('#company').val(),
+            education: $('#education').val(),
+            military_veteran: $('#military_veteran').val(),
+            receive_news: $('#receive_news').val(),
+            list_ids: $('#list_ids').val(),
+            linkedin: linkedinProfile,
         };
-		console.log(formData);
-		console.log(apiConfig.apiBaseUrl);
+
+        // Show loading message while processing registration
+        Swal.fire({
+            title: 'Please wait...',
+            text: 'We are verifying your data. This may take a moment.',
+            allowOutsideClick: false,
+            didOpen: () => Swal.showLoading()
+        });
+
+        // Make the API request to register the form data
         return fetch(`${apiConfig.apiBaseUrl}/api/typ/register`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
         })
-        .then(response => response.json())
-        .then(() => {
-            Swal.fire({
-                title: 'Success!',
-                text: 'Registered successfully!',
-                icon: 'success'
+            .then(response => response.json())
+            .then(() => {
+                // Close loading message
+                Swal.close();
+
+                // Show success message
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Registered successfully!',
+                    icon: 'success',willClose: () => {
+                        //reload the page
+                        location.reload();
+                    }
+                });
+            })
+            .catch(error => {
+                // Show validation error message if the request fails
+                Swal.showValidationMessage(`Request failed: ${error}`);
             });
-        })
-        .catch(error => {
-            Swal.showValidationMessage(`Request failed: ${error}`);
-        });
     }
+
 
     // Initialization
     $(document).ready(function() {
